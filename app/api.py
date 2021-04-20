98e06913ac40ad769511c3cfa256c66f3710a45f
juandavid716 <jbojato@uninorte.edu.co>
2021-04-19 22:48:11 -0500

    fix: json format

--- app/api.py
+++ app/api.py
@@ -6,7 +6,7 @@ class Password(Resource):
 
     def get_Pass(self,passw):
         password = main(passw)
-        return {'num_it': password}
+        return [{'num_it': password}]
 
     def get(self, password):
         return self.get_Pass(password)
